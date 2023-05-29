4c219caeaeadd1b8978683396169e10c22d1e0b3
Noman Aijaz <nomikk08@gmail.com>
2023-05-28 22:00:45 -0700

    adding blog webiste to github

---
+++ my_site/blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Author, Tag, Post
+
+# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "date", "author",)
+    list_filter = ("author", "tags", "date",)
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Author)
+admin.site.register(Tag)
+admin.site.register(Post, PostAdmin)
