9770ad4190fd13650f231dfcd171946268f06f68
Noman Aijaz <nomikk08@gmail.com>
2023-06-01 03:43:58 -0700

    Handled comment-form submission and output comments

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,7 +1,9 @@
 from typing import Any, Dict
 from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, View
+from django.http import HttpResponseRedirect
+from django. urls import reverse
 
 from .models import Post
 from .forms import CommentForm
@@ -27,12 +29,34 @@ class AllPostsVeiw(ListView):
     context_object_name = 'posts'
 
 
-class PostDetailView(DetailView):
+class PostDetailView(View):
     model = Post
     template_name = 'blog/post-detail.html'
 
-    def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
-        context['post_tags'] = self.object.tags.all()
-        context['comment_form'] = CommentForm()
-        return context
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            'post': post,
+            'post_tags': post.tags.all(),
+            'comment_form': CommentForm(),
+            'comments': post.comments.all().order_by('-id')
+        }
+        return render(request, 'blog/post-detail.html', context)
+    
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug = slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse('post-detail-page', args=[slug]))
+
+        context = {
+            'post': post,
+            'post_tags': post.tags.all(),
+            'comment_form': CommentForm,
+            'comments': post.comments.all()
+        }
+        return render(request, 'blog/post-detail.html', context)
