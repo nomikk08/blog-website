4bb6cc9bb5e1a1e085cc7e9efa3b2d0101d5f0a7
Noman Aijaz <nomikk08@gmail.com>
2023-06-01 01:26:21 -0700

    Added a comment form

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -2,7 +2,9 @@ from typing import Any, Dict
 from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
+
 from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -17,11 +19,13 @@ class StartingPageView(ListView):
         data = queryset[:3]
         return data
 
+
 class AllPostsVeiw(ListView):
     model = Post
     template_name = 'blog/all-posts.html'
     ordering = ['-date']
     context_object_name = 'posts'
+
 
 class PostDetailView(DetailView):
     model = Post
@@ -30,4 +34,5 @@ class PostDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context =  super().get_context_data(**kwargs)
         context['post_tags'] = self.object.tags.all()
+        context['comment_form'] = CommentForm()
         return context
