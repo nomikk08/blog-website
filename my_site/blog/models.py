14b2a55620b411b9beef4c7811d4d8ba26421eb4
Noman Aijaz <nomikk08@gmail.com>
2023-06-01 00:56:29 -0700

    Added a comment model

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -29,3 +29,9 @@ class Post(models.Model):
     context = models.TextField(validators=[MinLengthValidator(100)])
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='posts')
     tags = models.ManyToManyField(Tag)
+
+class Comments(models.Model):
+    user_name = models.CharField(max_length=100)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
